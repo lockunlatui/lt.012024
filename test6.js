137574e542e875526acf827f3a488bc0f1713348
MSI\OS <tuan120602@gmail.com>
2024-01-23 21:18:54 +0700

    add test 6

---
+++ test6.js
@@ -0,0 +1,10 @@
+const addCart = document.getElementById("addCart");
+function changeText() {
+    addCart.innerHTML = "Added";
+    addCart.style.color = "white";
+    addCart.style.fontWeight = "bold";
+}
+
+var x = 6;
+var x = "Loc";
+var x = true;
